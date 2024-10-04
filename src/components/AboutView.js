65cef9065301d2ba93439b5bb6637b7113f97b9f
Shubhrajit Santra <shubhrajit.santra@gmail.com>
2024-10-04 19:38:05 +0530

    Changing text content and text alignment of Home page and About page

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -10,6 +10,9 @@ const AboutView = () => {
             <p className="lead">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet numquam asperiores, dolore! Animi, veritatis maiores. Vel magni dolorum, vitae eum ut culpa unde autem blanditiis voluptatibus tenetur deleniti suscipit officiis non molestias nam reiciendis. Corporis cupiditate non sapiente inventore, illum eum aperiam, veniam numquam adipisci nisi. Necessitatibus, sint aliquid iusto.
             </p>
+            <p className="lead">
+                Vel magni dolorum, vitae eum ut culpa unde autem blanditiis voluptatibus tenetur deleniti suscipit officiis non molestias nam reiciendis. Corporis cupiditate non sapiente inventore, illum eum aperiam, veniam numquam adipisci nisi.
+            </p>
           </div>
         </div>
       </div>
