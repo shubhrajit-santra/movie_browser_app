167e45dbf397d10d63263657fbeca6eb6bda8a29
Shubhrajit Santra <shubhrajit.santra@gmail.com>
2025-08-31 22:33:01 +0530

    Changes to deploy the Movie Browser App on GitHub Pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/movie_browser_app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
